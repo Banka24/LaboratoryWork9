a3fb813e27b4294611fa339c4302e5c416cb414a
Banka24 <Aq567489012@gmail.com>
2024-02-09 20:37:49 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        private static double GetSum(int x, int n = 9)
+        {
+            if (n == 0) return Math.Sin(n * x) / (n + 2.0);
+
+            return Math.Sin(n * x) / (n + 2.0) + GetSum(x, --n);
+        }
+        private static void Main()
+        {
+            int x = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{GetSum(x)}");
+        }
+    }
+}
